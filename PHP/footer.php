ee60e998b834d5845acf9a54ef769d4184362001
Jonathan Costa <jonathancosta2100@gmail.com>
2021-10-20 10:49:34 -0300

    add layout livros favoritos na home

--- PHP/footer.php
+++ PHP/footer.php
@@ -1,7 +1,6 @@
 <footer>
     <p>Objetivo é treinar minhas skills com HTML, Css, JavaScript, PHP e Banco de dados</p>
 </footer>
-</div>
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
--- PHP/index.php
+++ PHP/index.php
@@ -1,62 +1,109 @@
 <?php
 include 'header.php';
 ?>
-<div class="container">
-  <?php
-  include 'menu.php';
-  ?>
-  <main>
-    <div class="conteudo">
-      <h1 class="titulo-h1">Organize sua leitura por aqui</h1><br><br>
-      <p class="titulo-p">&nbsp;&nbsp;&nbsp;&nbsp;Site criado para listar seus livros, Novels, Manga, Manhwa, Manhua que esta lendo, já
-        leu
-        ou gostaria de ler.</p>
-      <p class="titulo-p"><br>
-        &nbsp;&nbsp;&nbsp;&nbsp;Lorem ipsum sem torquent cubilia dapibus bibendum quis viverra dolor, aenean felis eros phasellus sapien morbi
-        nisi vel venenatis vivamus, neque commodo molestie quam curae pretium euismod ante.
-        fringilla phasellus malesuada hendrerit per dui egestas ante netus dictum leo interdum pharetra, lacus blandit
-        vulputate scelerisque non iaculis fringilla sem viverra amet dictum, curae facilisis litora conubia
-        pellentesque eros ac rhoncus iaculis pharetra aenean.
-        risus cubilia cras curabitur nunc pellentesque curae tempus aenean, donec himenaeos nec vehicula facilisis
-        tempus nullam lacinia, urna litora pulvinar porttitor eget sit massa.
-        eget laoreet ornare nam varius eros lacinia velit cursus suscipit tempus, felis aliquam ligula tellus mauris
-        himenaeos aliquam duis lectus ac faucibus, pulvinar velit commodo justo accumsan sociosqu risus est consequat.
-      </p>
-      <p class="titulo-p">
-        &nbsp;&nbsp;&nbsp;&nbsp;Mauris a quisque sapien varius nam ullamcorper maecenas sollicitudin habitasse purus, ultricies quam curae
-        viverra interdum aliquam donec etiam facilisis tempor sem, euismod tempus eleifend metus et lacus semper
-        consectetur at.
-        ac turpis non sollicitudin molestie proin fusce tellus venenatis nostra cubilia eu nulla, urna nisi blandit
-        urna donec ut eleifend dictum lobortis amet.
-        curabitur litora condimentum ultrices facilisis vitae tristique vehicula etiam elementum potenti vitae nec
-        orci taciti, ligula eu vitae nibh posuere diam euismod tortor suspendisse faucibus magna tincidunt arcu.
-        auctor mollis non egestas malesuada laoreet aliquam sagittis consequat, semper tincidunt suspendisse curabitur
-        eros phasellus dui, pharetra quisque vivamus orci scelerisque urna inceptos.
-      </p>
-      <p class="titulo-p">
-        &nbsp;&nbsp;&nbsp;&nbsp;Primis risus sed volutpat aliquam ultrices fames litora egestas scelerisque, iaculis pretium malesuada nullam
-        venenatis aliquet aliquam vehicula.
-        fames diam fermentum ut ac ultrices ornare nam inceptos scelerisque congue, curae massa leo ipsum in curabitur
-        orci nisl habitasse.
-        sem dolor suscipit leo justo malesuada etiam praesent, mi himenaeos eget fringilla senectus fringilla
-        interdum, ullamcorper non placerat phasellus primis etiam.
-        semper ullamcorper ac proin orci quisque condimentum ligula, fringilla arcu vestibulum id egestas pellentesque
-        integer fringilla, fermentum nulla interdum et consequat suscipit.
-        duis elit ut id mollis molestie nunc habitant praesent, enim pulvinar maecenas mauris dictumst torquent purus,
-        vivamus ipsum tortor tincidunt class praesent pellentesque.
-      </p>
-      <p class="titulo-p">
-        &nbsp;&nbsp;&nbsp;&nbsp;Vehicula blandit iaculis ullamcorper rutrum tellus erat nibh tincidunt, sociosqu senectus sed leo ut purus
-        bibendum nunc, eu imperdiet cras nibh orci pharetra ante.
-        in proin taciti orci tortor conubia, felis eu purus lacinia class hendrerit, dui dictum tempor sem.
-        morbi malesuada metus accumsan dictumst elit tristique nisi ad class leo fames eleifend, conubia pharetra mi
-        id donec dolor a integer hac curabitur maecenas etiam fermentum, a curabitur eleifend accumsan turpis semper
-        convallis vel tortor suscipit donec.
-        dictum et dictum sollicitudin orci fermentum arcu consequat lacinia hac eu, ultrices cubilia volutpat fames
-        nam id vehicula nec purus ultrices, odio tempor senectus primis rhoncus class est porta scelerisque.
-      </p>
-    </div>
-  </main>
-  <?php
-  include 'footer.php';
-  ?>
+<?php
+include 'menu.php';
+?>
+<main>
+  <div class="conteudo">
+    <h1 class="titulo-h1">Organize sua leitura por aqui</h1><br><br>
+    <p class="titulo-p">&nbsp;&nbsp;&nbsp;&nbsp;Site criado para listar seus livros, Novels, Manga, Manhwa, Manhua que esta lendo, já
+      leu
+      ou gostaria de ler.</p>
+    <p class="titulo-p"><br>
+      &nbsp;&nbsp;&nbsp;&nbsp;Lorem ipsum sem torquent cubilia dapibus bibendum quis viverra dolor, aenean felis eros phasellus sapien morbi
+      nisi vel venenatis vivamus, neque commodo molestie quam curae pretium euismod ante.
+      fringilla phasellus malesuada hendrerit per dui egestas ante netus dictum leo interdum pharetra, lacus blandit
+      vulputate scelerisque non iaculis fringilla sem viverra amet dictum, curae facilisis litora conubia
+      pellentesque eros ac rhoncus iaculis pharetra aenean.
+      risus cubilia cras curabitur nunc pellentesque curae tempus aenean, donec himenaeos nec vehicula facilisis
+      tempus nullam lacinia, urna litora pulvinar porttitor eget sit massa.
+      eget laoreet ornare nam varius eros lacinia velit cursus suscipit tempus, felis aliquam ligula tellus mauris
+      himenaeos aliquam duis lectus ac faucibus, pulvinar velit commodo justo accumsan sociosqu risus est consequat.
+    </p>
+    <p class="titulo-p">
+      &nbsp;&nbsp;&nbsp;&nbsp;Mauris a quisque sapien varius nam ullamcorper maecenas sollicitudin habitasse purus, ultricies quam curae
+      viverra interdum aliquam donec etiam facilisis tempor sem, euismod tempus eleifend metus et lacus semper
+      consectetur at.
+      ac turpis non sollicitudin molestie proin fusce tellus venenatis nostra cubilia eu nulla, urna nisi blandit
+      urna donec ut eleifend dictum lobortis amet.
+      curabitur litora condimentum ultrices facilisis vitae tristique vehicula etiam elementum potenti vitae nec
+      orci taciti, ligula eu vitae nibh posuere diam euismod tortor suspendisse faucibus magna tincidunt arcu.
+      auctor mollis non egestas malesuada laoreet aliquam sagittis consequat, semper tincidunt suspendisse curabitur
+      eros phasellus dui, pharetra quisque vivamus orci scelerisque urna inceptos.
+    </p>
+    <p class="titulo-p">
+      &nbsp;&nbsp;&nbsp;&nbsp;Primis risus sed volutpat aliquam ultrices fames litora egestas scelerisque, iaculis pretium malesuada nullam
+      venenatis aliquet aliquam vehicula.
+      fames diam fermentum ut ac ultrices ornare nam inceptos scelerisque congue, curae massa leo ipsum in curabitur
+      orci nisl habitasse.
+      sem dolor suscipit leo justo malesuada etiam praesent, mi himenaeos eget fringilla senectus fringilla
+      interdum, ullamcorper non placerat phasellus primis etiam.
+      semper ullamcorper ac proin orci quisque condimentum ligula, fringilla arcu vestibulum id egestas pellentesque
+      integer fringilla, fermentum nulla interdum et consequat suscipit.
+      duis elit ut id mollis molestie nunc habitant praesent, enim pulvinar maecenas mauris dictumst torquent purus,
+      vivamus ipsum tortor tincidunt class praesent pellentesque.
+    </p>
+    <p class="titulo-p">
+      &nbsp;&nbsp;&nbsp;&nbsp;Vehicula blandit iaculis ullamcorper rutrum tellus erat nibh tincidunt, sociosqu senectus sed leo ut purus
+      bibendum nunc, eu imperdiet cras nibh orci pharetra ante.
+      in proin taciti orci tortor conubia, felis eu purus lacinia class hendrerit, dui dictum tempor sem.
+      morbi malesuada metus accumsan dictumst elit tristique nisi ad class leo fames eleifend, conubia pharetra mi
+      id donec dolor a integer hac curabitur maecenas etiam fermentum, a curabitur eleifend accumsan turpis semper
+      convallis vel tortor suscipit donec.
+      dictum et dictum sollicitudin orci fermentum arcu consequat lacinia hac eu, ultrices cubilia volutpat fames
+      nam id vehicula nec purus ultrices, odio tempor senectus primis rhoncus class est porta scelerisque.
+    </p>
+  </div>
+  <div class="livroFavorito">
+    <fieldset class="container">
+      <legend id="legendLivro">
+        <h1><b>Livros, Novels, Manga, Manhwa, Manhua <br> mais lidos</b></h1>
+      </legend>
+      <div class="listaLivroFav">
+        <img class="imgLivroFav" src="../IMG/iconePagina.jpg" alt="">
+        <div class="conLivroFav">
+          <h1 class="titulo-h1">
+            <a href="#">titulo livro</a>
+
+          </h1>
+          <span class="icone">
+            <ion-icon name="add-circle-outline"></ion-icon>
+          </span>
+          Adicionar
+        </div>
+      </div>
+      <!--  -->
+      <div class="listaLivroFav">
+        <img class="imgLivroFav" src="../IMG/iconePagina.jpg" alt="">
+        <div class="conLivroFav">
+          <h1 class="titulo-h1">
+            <a href="#">titulo livro</a>
+
+          </h1>
+          <span class="icone">
+            <ion-icon name="add-circle-outline"></ion-icon>
+          </span>
+          Adicionar
+        </div>
+      </div>
+      <!--  -->
+      <div class="listaLivroFav">
+        <img class="imgLivroFav" src="../IMG/iconePagina.jpg" alt="">
+        <div class="conLivroFav">
+          <h1 class="titulo-h1">
+            <a href="#">titulo livro</a>
+
+          </h1>
+          <span class="icone">
+            <ion-icon name="add-circle-outline"></ion-icon>
+          </span>
+          Adicionar
+        </div>
+      </div>
+    </fieldset>
+  </div>
+</main>
+<?php
+include 'footer.php';
+?>
