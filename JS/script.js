e67e57f60310842e8032f62e5a9252ae1a60ad89
Jonathan Costa <jonathancosta2100@gmail.com>
2021-10-23 18:17:47 -0300

    botão para copiar o email

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+document.getElementById("botao-copiar-email").addEventListener("click", function () {
+
+    document.getElementById("texto").select();
+    document.execCommand('copy');
+});
+/* <img class="icone-contato" src="https://cdn-icons-png.flaticon.com/512/281/281769.png"> </img>*/
--- PHP/footer.php
+++ PHP/footer.php
@@ -19,15 +19,16 @@
                 <a href="https://github.com/JonathanSantos2000">
                     <img class="icone-contato" src="https://cdn-icons-png.flaticon.com/512/779/779088.png">
                 </a>
-                <a href="https://github.com/JonathanSantos2000">
+                <button id="botao-copiar-email">
                     <img class="icone-contato" src="https://cdn-icons-png.flaticon.com/512/281/281769.png">
-                </a>
+                </button>
             </div>
         </div>
     </div>
 </footer>
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<script src="../JS/script.js"></script>
 </body>
 
 </html>
